adcab2d50b122a0737554634c005fabdb1fa67a8
anisamuema <anisa.muema.school@gmail.com>
2023-09-20 21:28:53 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
-// Write your code here
+// Write your code
+let num1 = 31;
+let num2 = 2;
+let multiply = 31*2;
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+let num3 = 20;
+let num4 = 5;
+let mod = 20/5;
+
+let max = 20;
